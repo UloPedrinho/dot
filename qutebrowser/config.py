17d545788736060ee9301d8a1da8fd5f18b5580d
pedro <pedro@montgomery.galiza>
2020-03-15 00:53:58 +0100

    * qutebrowser/config.py: added

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -11,11 +11,24 @@ dracula.draw.blood(c, {
     }
 })
 
+# autosave
+c.auto_save.session = True
+
 # tabs
 c.colors.tabs.selected.even.bg = "#5d478b"
 c.colors.tabs.selected.odd.bg = "#5d478b"
+c.colors.tabs.pinned.selected.even.bg = "#5d478b"
+c.colors.tabs.pinned.selected.odd.bg = "#5d478b"
 
 c.tabs.padding = {'top': 0, 'bottom': 0, 'left': 0, 'right': 0}
+
+# status bar
+c.statusbar.padding = {'top': 0, 'bottom': 0, 'left': 0, 'right': 0}
+
+# webpage
+# requires Qt1.5..
+# c.colors.webpage.prefers_color_scheme_dark = True
+c.qt.args = ["blink-settings=darkMode=4"]
 
 # keybindings
 config.bind('<Ctrl-g>', 'leave-mode')
