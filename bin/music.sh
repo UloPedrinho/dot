c0a0da5f8eb9bda3ea97a37cf2be90ba3ee4f46c
pedro <pedro@montgomery.galiza>
2020-09-08 13:28:29 +0200

    sxhkdrc 'ntilde' fixed. '-m 1' option

--- bin/music.sh
+++ bin/music.sh
@@ -1,5 +1,6 @@
+
 #!/bin/sh
 
 MUSIC_DIR=$HOME/user/media/music/songs/
 
-mpv --title='music.sh || ${filename}' --force-window=yes --shuffle $MUSIC_DIR  --input-ipc-server=/tmp/mpvsocket &
+mpv --title='music.sh || ${filename}' --force-window=yes --script-opts=osc-visibility=always --shuffle --pause $MUSIC_DIR --input-ipc-server=/tmp/mpvsocket &
