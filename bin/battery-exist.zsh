263912ae72a6a347e39a36e39b57301105ef697f
pedro <pedro@montgomery.galiza>
2020-10-03 17:21:39 +0200

    * i3/i3blocks.conf: renamed battery script to .zsh

--- bin/battery-exist.zsh
+++ bin/battery-exist.zsh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/bin/zsh
 
 BATTERY=$(upower --enumerate| grep -i "battery_BAT")
 
-if [[ $BATTERY == 0]]
+if [[ $? == 0 ]]
 then
     BATX=$(upower --enumerate| grep -i "battery_BAT" | rev | cut -d "/" -f1 )
     POWER=$(acpi -b | grep "${BATX[1]}:" | grep -E -o '[0-9]+[0-9][0-9]?%')
     echo "Battery: $POWER"
     echo "BAT: $POWER"
 
-    [ ${BAT%?} -le 5 ] && exit 33
-    [ ${BAT%?} -le 20 ] && echo "#FF8000"
+    [[ ${POWER%?} -le 5 ]] && exit 33
+    [[ ${POWER%?} -le 20 ]] && echo "#FF8000"
+    [[ ${POWER%?} -ge 60 ]] && echo "#00ff00"
 fi
 
 exit 0
