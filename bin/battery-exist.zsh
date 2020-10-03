c5458f5d393530a35c00bd3db5c2cfda1e638d57
pedro <pedro@montgomery.galiza>
2020-10-03 20:33:37 +0200

    * bin/battery-exist.zsh: changed battery string

--- bin/battery-exist.zsh
+++ bin/battery-exist.zsh
@@ -6,7 +6,7 @@ if [[ $? == 0 ]]
 then
     BATX=$(upower --enumerate| grep -i "battery_BAT" | rev | cut -d "/" -f1 )
     POWER=$(acpi -b | grep "${BATX[1]}:" | grep -E -o '[0-9]+[0-9][0-9]?%')
-    echo "Battery: $POWER"
+    echo "$POWER"
     echo "BAT: $POWER"
 
     [[ ${POWER%?} -le 5 ]] && exit 33
