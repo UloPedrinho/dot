b2b835b2798ddb728753b9b9f74e4e884df0c879
pedro <pedro@montgomery.galiza>
2021-01-11 04:14:24 +0100

    * bin/rofi.zsh: set big font to show big icons

--- bin/rofi.zsh
+++ bin/rofi.zsh
@@ -8,4 +8,4 @@ else
     SHOW="window"
 fi
 
-rofi -show-icons -modi $DATA -show $SHOW -width 40 -location 0 -lines 15 -bw 0 -yoffset 0 -color-normal "#050505,#3a5fcd,#050505,#383838,#eee8d5" -color-window "#050505,#3a5fcd,#000000"
+rofi -show-icons -modi $DATA -show $SHOW -width 40 -location 0 -lines 15 -font "Hack 16" -bw 0 -yoffset 0 -color-normal "#050505,#3a5fcd,#050505,#383838,#eee8d5" -color-window "#050505,#3a5fcd,#000000"
